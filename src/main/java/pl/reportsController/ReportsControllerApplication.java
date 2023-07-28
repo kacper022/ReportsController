bedde96576b271e485955d3d68dde6717561f135
Kacper <kacper.kuczminski@gmail.com>
2023-07-28 17:39:13 +0200

    Poprawka dla przesyłania danych z/do frontendu

--- src/main/java/pl/reportsController/ReportsControllerApplication.java
+++ src/main/java/pl/reportsController/ReportsControllerApplication.java
@@ -2,12 +2,19 @@ package pl.reportsController;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ReportsControllerApplication {
+public class ReportsControllerApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportsControllerApplication.class, args);
 	}
-
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000")
+				.allowedMethods("GET","POST","PUT","DELETE");
+	}
 }
